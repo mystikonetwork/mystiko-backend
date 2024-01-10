a27b9e812712c085f27e4d3485fb6442aed17a92
moomarco <iamwumaixing@gmail.com>
2024-01-10 17:57:36 +0800

    impl TransactionMiddleware trait for Box

--- mystiko_server_utils/src/tx_manager/types.rs
+++ mystiko_server_utils/src/tx_manager/types.rs
@@ -27,3 +27,33 @@ pub trait TransactionMiddleware<P: JsonRpcClient>: Debug + Send + Sync {
         provider: &Provider<P>,
     ) -> TransactionMiddlewareResult<TransactionReceipt>;
 }
+
+#[async_trait::async_trait]
+impl<P> TransactionMiddleware<P> for Box<dyn TransactionMiddleware<P>>
+where
+    P: JsonRpcClient,
+{
+    fn tx_eip1559(&self) -> bool {
+        self.as_ref().tx_eip1559()
+    }
+
+    async fn gas_price(&self, provider: &Provider<P>) -> TransactionMiddlewareResult<U256> {
+        self.as_ref().gas_price(provider).await
+    }
+
+    async fn estimate_gas(&self, data: &TransactionData, provider: &Provider<P>) -> TransactionMiddlewareResult<U256> {
+        self.as_ref().estimate_gas(data, provider).await
+    }
+
+    async fn send(&self, data: &TransactionData, provider: &Provider<P>) -> TransactionMiddlewareResult<TxHash> {
+        self.as_ref().send(data, provider).await
+    }
+
+    async fn confirm(
+        &self,
+        tx_hash: &TxHash,
+        provider: &Provider<P>,
+    ) -> TransactionMiddlewareResult<TransactionReceipt> {
+        self.as_ref().confirm(tx_hash, provider).await
+    }
+}
