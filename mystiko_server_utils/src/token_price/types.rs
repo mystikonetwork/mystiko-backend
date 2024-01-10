5765508cc36329000dc7f3df423e33342e58a389
X <96100679+moomarco@users.noreply.github.com>
2024-01-10 17:26:03 +0800

    impl PriceMiddleware trait for Box (#14)
    * impl PriceMiddleware trait for Box
    
    * update
    
    * update

--- mystiko_server_utils/src/token_price/types.rs
+++ mystiko_server_utils/src/token_price/types.rs
@@ -1,4 +1,5 @@
 use crate::token_price::PriceMiddlewareError;
+use async_trait::async_trait;
 use ethers_core::types::U256;
 use std::fmt::Debug;
 
@@ -15,4 +16,24 @@ pub trait PriceMiddleware: Debug + Send + Sync {
         asset_b: &str,
         decimal_b: u32,
     ) -> PriceMiddlewareResult<U256>;
+}
+
+#[async_trait]
+impl PriceMiddleware for Box<dyn PriceMiddleware> {
+    async fn price(&self, symbol: &str) -> PriceMiddlewareResult<f64> {
+        self.as_ref().price(symbol).await
+    }
+
+    async fn swap(
+        &self,
+        asset_a: &str,
+        decimal_a: u32,
+        amount_a: U256,
+        asset_b: &str,
+        decimal_b: u32,
+    ) -> PriceMiddlewareResult<U256> {
+        self.as_ref()
+            .swap(asset_a, decimal_a, amount_a, asset_b, decimal_b)
+            .await
+    }
 }
